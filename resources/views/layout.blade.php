6952f99f8752d61bc1887dc5a9cbba17ad264a2f
Maikol98 <56239415+Maikol98@users.noreply.github.com>
2021-10-15 23:38:46 -0400

    layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
+    <title>Hello, world!</title>
+  </head>
+  <body>
+        <header style="background-color: rebeccapurple">
+            <nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgba(255, 0, 0, 0)">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Navbar</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                      <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      <a class="nav-link" href="#">Features</a>
+                      <a class="nav-link" href="#">Pricing</a>
+                      <a class="nav-link disabled">Disabled</a>
+                    </div>
+                  </div>
+                </div>
+            </nav>
+            <div class="row">
+                <div class="col-2">
+                    <h1>Prueba Ahora <br> Un nuevo estilo</h1>
+                    <p>El Código fair play de la FIFA abraza todos los principios deportivos, morales y éticos que defiende 
+                        la FIFA, y por los cuales continuará 
+                        luchando en el futuro, independientemente de las influencias y presiones que pueda afrontar.
+                    </p>
+                    <a href="" class="btn">Explorar Ahora &#8594;</a>
+                </div>
+                <div class="col-2">
+                    <img src="{{ asset('asset/img/image1.png') }}" alt="">
+                </div>
+            </div>
+        </header>
+    
+    <footer>
+        <div class="row">
+            <div class="footer-col-1">
+                <h3>Descarga nuestra App</h3>
+                <p>Descarga la App android y IOS en tu Smartphone.</p>
+                <div class="app-logo">
+                    <img src="images/play-store.png" >
+                    <img src="images/app-store.png" >
+                </div>
+            </div>
+            <div class="footer-col-2">
+                <img src="images/logo-white.png" >
+                <p>Nuestro proposito es hacer de forma sostenible el 
+                    placer del deporte accesible al dinero. 
+                </p>
+            </div>
+            <div class="footer-col-3">
+                <h3>Enlaces utiles</h3>
+                <ul>
+                    <li>Cupones</li>
+                    <li>Blogs</li>
+                    <li>Unete al Afiliado </li>
+                    <li>Politica de Devoluciones</li>
+                </ul>
+            </div>
+            <div class="footer-col-4">
+                <h3>Redes Sociales</h3>
+                <ul>
+                    <li><i class="fa fa-facebook"> </i></li>
+                    <li><i class="fa fa-twitter"></i></li>
+                    <li><i class="fa fa-instagram"></i></li>
+                    <li><i class="fa fa-youtube"></i></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <p class="author"> author: Erick Vidal-2021</p>
+    </footer>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
